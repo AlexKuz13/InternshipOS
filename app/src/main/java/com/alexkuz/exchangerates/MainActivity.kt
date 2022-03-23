20f56db9fa24c2b19b6af5e6509ae4ce734925ed
AlexKuz13 <kuznetsov356@gmail.com>
2022-03-23 19:45:35 +0300

    #81116 Add Splash Screen

--- app/src/main/java/com/alexkuz/exchangerates/MainActivity.kt
+++ app/src/main/java/com/alexkuz/exchangerates/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_ExchangeRates)
         setContentView(R.layout.activity_main)
     }
 }
