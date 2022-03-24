18a6fabaf497eba865364cbd782ee41a9ebfa1c4
AlexKuz13 <kuznetsov356@gmail.com>
2022-03-24 16:56:53 +0300

    #81116 Add Date Picker Dialog

--- app/src/main/java/com/alexkuz/exchangerates/ui/fragments/CurrencyListFragment.kt
+++ app/src/main/java/com/alexkuz/exchangerates/ui/fragments/CurrencyListFragment.kt
@@ -1,13 +1,16 @@
 package com.alexkuz.exchangerates.ui.fragments
 
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.alexkuz.exchangerates.R
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.alexkuz.exchangerates.databinding.FragmentCurrencyListBinding
+import com.alexkuz.exchangerates.util.CustomDatePicker
+import java.text.DateFormatSymbols
+import java.util.*
 
 
 class CurrencyListFragment : Fragment() {
@@ -15,18 +18,44 @@ class CurrencyListFragment : Fragment() {
     private var _binding: FragmentCurrencyListBinding? = null
     private val binding get() = _binding!!
 
+    private var cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+    private val year get() = cal.get(Calendar.YEAR)
+    private val month get() = cal.get(Calendar.MONTH)
+    private val day get() = cal.get(Calendar.DAY_OF_MONTH)
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentCurrencyListBinding.inflate(inflater,container,false)
-        return binding.root
+        _binding = FragmentCurrencyListBinding.inflate(inflater, container, false)
 
+        binding.tieDate.setText(convertDate(day, month, year))
+
+        binding.tieDate.setOnClickListener { openDatePicker() }
+
+        return binding.root
     }
 
+    private fun openDatePicker() {
+        val datePicker = CustomDatePicker(cal).create()
+
+        datePicker.addOnPositiveButtonClickListener {
+            cal.timeInMillis = it
+            binding.tieDate.setText(convertDate(day, month, year))
+        }
+
+        datePicker.show(childFragmentManager, "ds")
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+    private fun convertDate(day: Int, month: Int, year: Int): String {
+        return "$day " + DateFormatSymbols().months[month] + " $year"
+    }
+
 }
